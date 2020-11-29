5df8a113a3337d818d2dfeea022e4a3e23a44a9e
Rabea Gleissner <rabeagleissner@gmail.com>
2020-11-29 16:13:00 +0800

    Add missing tests for existing functionality

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe(DateRangeFormatter) do
     expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
   end
 
+  it "formats a date range for the same day with ending time only" do
+    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", nil, "11:00")
+    expect(formatter.to_s).to eq("1st November 2009 until 11:00")
+  end
+
   it "formats a date range for the same month" do
     formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3")
     expect(formatter.to_s).to eq("1st - 3rd November 2009")
@@ -29,6 +34,11 @@ RSpec.describe(DateRangeFormatter) do
   it "formats a date range for the same month with starting and ending times" do
     formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00", "11:00")
     expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009 at 11:00")
+  end
+
+  it "formats a date range for the same month with ending time only" do
+    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", nil, "11:00")
+    expect(formatter.to_s).to eq("1st November 2009 - 3rd November 2009 at 11:00")
   end
 
   it "formats a date range for the same year" do
@@ -46,6 +56,11 @@ RSpec.describe(DateRangeFormatter) do
     expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 1st December 2009 at 11:00")
   end
 
+  it "formats a date range for the same year with ending time only" do
+    formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1", nil, "11:00")
+    expect(formatter.to_s).to eq("1st November 2009 - 1st December 2009 at 11:00")
+  end
+
   it "formats a date range for different year" do
     formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1")
     expect(formatter.to_s).to eq("1st November 2009 - 1st December 2010")
@@ -59,6 +74,11 @@ RSpec.describe(DateRangeFormatter) do
   it "formats a date range for different year with starting and ending times" do
     formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1", "10:00", "11:00")
     expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 1st December 2010 at 11:00")
+  end
+
+  it "formats a date range for different year with ending time only" do
+    formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1", nil, "11:00")
+    expect(formatter.to_s).to eq("1st November 2009 - 1st December 2010 at 11:00")
   end
 end
 
